6daba6a3348ae7608dc2f54f448a4c73f694c8b9
Trung Nguyen <trungnguyen1221999@gmail.com>
2026-03-24 10:32:17 +0200

    init backend

---
+++ backend/eslint.config.js
@@ -0,0 +1,16 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+  }
+);
